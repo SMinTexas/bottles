4ce58676ac6b71d4ed4582f4dbb958636cc6144f
Steven Murray <tstevenm@gmail.com>
2019-08-29 16:59:20 -0500

    Update to play the song slower

--- funny_song.py
+++ funny_song.py
@@ -1,3 +1,5 @@
+import time
+
 def sing(n):
 	if (n == 1):
 		objects = 'bottle'
@@ -12,10 +14,12 @@ def sing(n):
 
 	if (n > 0):
 		print(str(n) + " " + objects + " of beer on the wall, " + str(n) + " " + objects + " of beer.")
+		time.sleep(2)
 		print("Take one down and pass it around, " + str(n-1) + " " + objectsMinusOne + " of beer on the wall.")
 		print(" ")
 	elif (n == 0):
 		print("No more bottles of beer on the wall, no more bottles of beer.")
+		time.sleep(2)
 		print("Go to the store and buy some more, 99 bottles of beer on the wall.")
 	else:
 		print("Error: Wheres the booze?")
